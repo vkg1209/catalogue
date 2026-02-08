ec1de66f2175bc89715501cdac9d80ea337b274a
vinay <gvk1209>
2026-02-08 14:31:34 +0530

    grhr

--- locals.tf
+++ locals.tf
@@ -3,6 +3,7 @@ locals {
     ami_id = data.aws_ami.joindevops.id
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
     common-tags = {
--- main.tf
+++ main.tf
@@ -137,7 +137,7 @@ resource "aws_autoscaling_group" "catalogue" {
     version = aws_launch_template.catalogue.latest_version
   }
 
-  vpc_zone_identifier       = local.private_subnet_id
+  vpc_zone_identifier       = local.private_subnet_ids
   target_group_arns = [aws_lb_target_group.catalogue.arn]
 
   instance_refresh {
